daa1ce421d424f252cff5142b7bc149687afb628
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2023-02-13 22:24:42 +0100

    routes done

---
+++ routes/api/users.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const userController = require("../../controller/users.js");
+const { validateData } = require("../../middlewares/validator.js");
+const { userValidate } = require("../../utils/validator.js");
+const { auth } = require("../../middlewares/authorizationJwt.js");
+const { upload } = require("../../middlewares/upload");
+
+const router = express.Router();
+
+router.post("/signup", validateData(userValidate), userController.register);
+
+router.post("/login", validateData(userValidate), userController.login);
+
+router.get("/logout", auth, userController.logout);
+
+router.get("/current", auth, userController.current);
+
+router.patch("/", auth, userController.updateSub);
+
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  userController.updateAvatar
+);
+
+router.get("/verify/:verificationToken", userController.verificationLink);
+
+router.post(
+  "/verify",
+  validateData(userValidate),
+  userController.repeatVerification
+);
+
+module.exports = router;
